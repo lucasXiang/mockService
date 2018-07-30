cea1c018cb72d400736d1539606fc2675cab60c1
xiangzhq <zhongqiang.xiang@ideacome.com>
2018-07-30 14:07:30 +0800

    update

--- src/main/java/com/ideacome/mock/controller/ConsumerController.java
+++ src/main/java/com/ideacome/mock/controller/ConsumerController.java
@@ -1,25 +1,27 @@
 package com.ideacome.mock.controller;
 
-import com.alibaba.fastjson.JSONObject;
-import com.ideacome.mock.MemCache.CacheOCS;
-import com.ideacome.mock.cache.CacheOS;
-import com.ideacome.mock.model.ChannelPushConfig;
-import com.ideacome.mock.returnDTO.ResultMsg;
-import com.ideacome.mock.service.ChannelPushConfigService;
-import org.apache.log4j.Logger;
-import org.apache.xerces.impl.dv.util.Base64;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+import com.ideacome.mock.MemCache.CacheOCS;
+import com.ideacome.mock.cache.CacheOS;
+import com.ideacome.mock.model.ChannelPushConfig;
+import com.ideacome.mock.returnDTO.ResultMsg;
+import com.ideacome.mock.service.ChannelPushConfigService;
 
 @RestController
 @RequestMapping("/mock")
@@ -41,9 +43,9 @@ public class ConsumerController {
 	            }
 				str = sb.toString();
 				if(str.contains("data")){
-					str = "{'data':"+new String(Base64.decode(JSONObject.parseObject(str).getString("data")),"utf-8")+"}";
+					str = "{'data':"+new String(Base64.decodeBase64(JSONObject.parseObject(str).getString("data")),"utf-8")+"}";
 				}else{
-					str = new String(Base64.decode(str),"utf-8");
+					str = new String(Base64.decodeBase64(str),"utf-8");
 				}
 			}else if(req.getMethod().equals(RequestMethod.GET.name())){
 				Map<String,String[]> map = (Map<String,String[]>)req.getParameterMap();
